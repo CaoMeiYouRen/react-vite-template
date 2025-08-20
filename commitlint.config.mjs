22318a31ba8f7109ffb094862d344e53599c9f3e
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-20 22:35:15 +0800

    build: 添加 commitlint 配置文件以规范化提交信息

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never'],
+        'body-max-line-length': [0, 'never'],
+        'footer-max-line-length': [0, 'never'],
+    },
+}
